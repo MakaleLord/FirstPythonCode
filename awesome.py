87ab4f36a15a1c55002b58acef3d9b05a884717b
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-28 17:19:10 -0500

    initial commit

---
+++ awesome.py
@@ -0,0 +1,46 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   min-width:55%;
+   max-width:100%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  white-space: pre;
+}
+</style>
+
+<div>
+Your total cost is... 148299000 dollars
+Whole Gaming Setup 30000
+Multiple Video Games 24000
+New Car 45000
+Gulfstream G450 Private Jet 16000000
+200ft Yacht 59000000
+Maldives Private Island 65000000
+New York Penthouse 4000000
+Abundance of clothing 200000
+Patek Watch 1000000
+Veneno Roadster 3000000
---
+++ car.py
@@ -0,0 +1,60 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   min-width:55%;
+   max-width:100%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  white-space: pre;
+}
+</style>
+
+<div>
+<b>1904 Buick Model B</b>
+
+The Buick Model B was Buick's first model. It was built in Jackson, Michigan and was introduced in 1905 at the New York Auto Show.It had a 2-cylinder, horizontally opposed engine the worlds first overhead valve engine.
+<pre>
+                                                            __......_
+                             .---.       _________    _.---`.-----_.'|
+                            ;     `.   (_   (_  _('\.' .--``   _.'   |
+                            `._//_.'    |   |   |   |.'     .-`     .'
+                  ,-. ,-.     //  (| _.' _.'_..-`   |..___.'        |
+         _       (O)_(O)_)_  //    ;(..(..(        .'     |        .'
+      ,-'"'--------` -| |-.\//     :|     |       /     '=|        |
+    ,'|`.            `---` \      |/___ /_......'        '-.....-`/
+    |I|II\________ .---.____|| __..'    /       |        /   ____  |
+  .'|I|II|        `----._`-.  `-..____.'        |____...`.-``    ```-..
+ /  |I|II|   .-````````-.`-.`.                         .'' .-``````-. ``.
+.  /`.|II|  ' .-``````-. `  \ `.                      / // \ || //  
+' .---.`.| / / \ || // \ \  \  \--------------------; : .   \||//   . : 
+ \ './/   . :----````----; '   | |==================|  ; ;----.__,----: : 
+    `-....; '   //||\   ' ;            `. `-....-` .'  \ '.// || \.' /
+   LGB     \ '.// || \.' /  .-.          `-......-' _.-.`. `-....-` .'
+       .-._ `. `-....-` .'  1904 Buick    .-.              `-......-'  -.
+   .-._       `-......-'    .-._                   _.-.        .-._
+
+
+</pre>
